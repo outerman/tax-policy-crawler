6875e7a51a7b8f4f35906394e7e12de728606f54
shenxy <xinyu_shen@163.com>
2017-09-15 10:09:16 +0800

    去除注释

--- TaxPolicyCrawlerScrapy/spiders/TaxPolicyExplainCrawler.py
+++ TaxPolicyCrawlerScrapy/spiders/TaxPolicyExplainCrawler.py
@@ -131,147 +131,19 @@ def parse_policy_detail(page_text, item):
 
     # 标题栏
     item['title'] = li_tags[0].text
-    # 发布日期
-    item['date'] = li_tags[1].text
-    # 来源
-    item['publisher'] = li_tags[1].text
+
+    date_and_publisher = li_tags[1].text
+
+    if str(date_and_publisher).__contains__('日期') and str(date_and_publisher).__contains__('来源'):
+        # 发布日期
+        item['date'] = str(date_and_publisher).split(' ')[0].strip()
+        # 来源
+        item['publisher'] = str(date_and_publisher).split(' ')[1].strip()
+    else:
+        # 发布日期
+        item['date'] = date_and_publisher
+        # 来源
+        item['publisher'] = date_and_publisher
 
     return item
 
-
-# # 从table的tr节里，获取文案
-# def get_text_in_tr(tr_tag, index):
-#     all_tds = tr_tag.find_all('td')
-#     if len(all_tds) <= index:
-#         return ''
-#
-#     td_str = all_tds[index]
-#
-#     return td_str.text
-#
-#
-# # 结果输出到Excel
-# def save_to_excel(policy_source, start_index, item_list):
-#     with lock:
-#         filename = 'TaxPolicyExplain.xls'
-#         sheet_name = '税收政策'
-#         is_reset = start_index == 0
-#         # 先删除目标文件
-#         if os.path.exists(filename) and is_reset:
-#             os.remove(filename)
-#
-#         if os.path.exists(filename):
-#             # 打开Excel
-#             rdbook = xlrd.open_workbook(filename)
-#             book = copy(rdbook)
-#             excel_sheet = book.get_sheet(0)
-#         else:
-#             # 生成导出文件
-#             book = xlwt.Workbook()
-#             excel_sheet = book.add_sheet(sheet_name)
-#
-#         # 标题行
-#         if is_reset:
-#             excel_sheet.write(0, 0, '序号')
-#             excel_sheet.write(0, 1, '政策来源')
-#             excel_sheet.write(0, 2, '政策类型')
-#             excel_sheet.write(0, 3, '税种')
-#             excel_sheet.write(0, 4, '标题')
-#             excel_sheet.write(0, 5, '副标题')
-#             excel_sheet.write(0, 6, '链接地址')
-#             excel_sheet.write(0, 7, '发文日期')
-#             excel_sheet.write(0, 8, '正文内容')
-#             excel_sheet.write(0, 9, '发文部门')
-#
-#         for i in range(1, len(item_list) + 1):
-#             row_item = item_list[i - 1]
-#             excel_sheet.write(start_index + i, 0, start_index + i)
-#             excel_sheet.write(start_index + i, 1, policy_source.source)
-#             excel_sheet.write(start_index + i, 2, policy_source.policyType)
-#             excel_sheet.write(start_index + i, 3, policy_source.taxLevel)
-#
-#             excel_sheet.write(start_index + i, 4, row_item.title)
-#             excel_sheet.write(start_index + i, 5, row_item.subtitle)
-#             excel_sheet.write(start_index + i, 6, row_item.url)
-#             excel_sheet.write(start_index + i, 7, row_item.date)
-#             excel_sheet.write(start_index + i, 8, row_item.content)
-#             excel_sheet.write(start_index + i, 9, row_item.publisher)
-#
-#         book.save(filename)
-#
-#
-# # 获取政策列表（分页）
-# def get_page_size(tr_tag):
-#     td_str = get_text_in_tr(tr_tag, 0)  # 获取第一个节点的字符串
-#     start = td_str.find('页 1/') + len('页 1/')
-#     if start < 0:
-#         return start
-#
-#     end = td_str.find(' ', start)
-#
-#     return int(td_str[start: end])
-#
-#
-# def crawl_by_page(index, page_size):
-#     try:
-#         start_time = time.time()
-#         policy_source = PolicySource('国税总局', '政策解读', '')
-#         request_util = get_request_util()  # 每页分开刷：不同代理、不同线程、重试单元
-#         print(threading.current_thread().name + ',获取第' + str(index + 1) + '/' + str(page_size) + '页的列表数据')
-#         item_list = get_item_list(request_util, index, page_size)  # 网站url从1开始
-#
-#         if not item_list:
-#             return False
-#
-#         policy_list_page = []
-#         for item in item_list:
-#             print(threading.current_thread().name + ',抓取网页：' + item.url)
-#             policy_list_page.append(get_policy_detail(request_util, item))
-#             # 不能频率太快，否则会被禁止访问, 随机延迟1~3秒
-#             time.sleep((1 + random.random() * 2))
-#
-#         # saveToExcel(policy_source, start_index, policy_list_page)
-#         # start_index += len(policy_list_page)
-#         save_to_excel(policy_source, index * 20 + 1, policy_list_page)
-#         print('finish crawling page ' + str(index) + '! take time:' + str(time.time() - start_time))
-#         return True
-#
-#     except Exception as ex:
-#         print('crawl_by_page generated an exception: %s' % str(ex))
-#         return False
-#
-#
-# def start_crawl():
-#     page_count = get_item_summary()
-#     print('page_size:' + str(page_count))
-#
-#     if not page_count or page_count <= 0:
-#         print('获取政策解读首页，失败！')
-#         return
-#
-#     # 测试一页
-#     # page_count = 2
-#
-#     start_time = time.time()
-#     # 每页分开刷：不同代理、不同线程、重试单元
-#     page_index_to_crawl = [i for i in range(page_count)]
-#     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-#         round_times = 1
-#         while len(page_index_to_crawl) > 0:
-#             print('crawl for %d th round' % round_times)
-#             future_to_crawl = {executor.submit(crawl_by_page, index, page_count): index for index in
-#                                page_index_to_crawl}
-#             for future in concurrent.futures.as_completed(future_to_crawl):
-#                 index = future_to_crawl[future]
-#                 try:
-#                     data = future.result()
-#                     if data:
-#                         page_index_to_crawl.remove(index)
-#                 except Exception as exc:
-#                     print('page %d crawl failed! %s' % (index, str(exc)))
-#             round_times += 1
-#         print('all complete! take time:' + str(time.time() - start_time))
-#
-#
-# # 程序启动
-# start_crawl()
