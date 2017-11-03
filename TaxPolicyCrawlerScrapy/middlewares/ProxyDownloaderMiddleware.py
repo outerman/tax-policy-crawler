18774c1fe71768f22f4cc292103ec01281ba08e9
shenxy <outerman@163.com>
2017-11-03 09:31:16 +0800

    不启用代理

--- TaxPolicyCrawlerScrapy/middlewares/ProxyDownloaderMiddleware.py
+++ TaxPolicyCrawlerScrapy/middlewares/ProxyDownloaderMiddleware.py
@@ -9,7 +9,9 @@ class ProxyDownloaderMiddleware(object):
     # 这个中间件，只需要确定proxy位置，放到meta['proxy']里，然后通过配置，由scrapy自带的downloadermiddleware.httpproxy来生效
     def process_request(self, request, spider):
         proxy = ProxyMgr.get_proxy()
-        if proxy.get('user_pass') is not None:
+        if proxy is None:
+            print("**************ProxyMiddleware no poxy************")
+        elif proxy.get('user_pass') is not None:
             request.meta['proxy'] = proxy['http']
             encoded_user_pass = base64.encodestring(proxy['user_pass'])
             request.headers['Proxy-Authorization'] = 'Basic ' + encoded_user_pass
--- TaxPolicyCrawlerScrapy/settings.py
+++ TaxPolicyCrawlerScrapy/settings.py
@@ -64,7 +64,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 1
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 CONCURRENT_REQUESTS_PER_IP = 16
@@ -129,3 +129,6 @@ ITEM_PIPELINES = {
 # HTTPCACHE_DIR = 'httpcache'
 # HTTPCACHE_IGNORE_HTTP_CODES = []
 # HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+# 是否使用代理服务器
+USE_PROXY = False
