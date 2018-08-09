3a6c788dfe5568a04a5d6dc3e3d393e5d2bdeb04
shenxy <outerman@163.com>
2018-08-09 14:54:55 +0800

    默认不使用代理

--- TaxPolicyCrawlerScrapy/settings.py
+++ TaxPolicyCrawlerScrapy/settings.py
@@ -64,9 +64,9 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 1
+DOWNLOAD_DELAY = 5
 # shenxy--是否使用代理服务器
-USE_PROXY = True
+USE_PROXY = False
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 CONCURRENT_REQUESTS_PER_IP = 16
